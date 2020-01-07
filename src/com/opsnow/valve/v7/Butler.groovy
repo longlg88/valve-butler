f12a86334b5de975464823fdcc07bd9c2ddeb042
longlg88 <longlg88@naver.com>
2020-01-07 07:06:14 +0000

    Test

--- src/com/opsnow/valve/v7/Butler.groovy
+++ src/com/opsnow/valve/v7/Butler.groovy
@@ -691,8 +691,8 @@ def npm_sonar(source_root = "", sonarqube = "") {
     }
     withCredentials([string(credentialsId: 'npm-sonar', variable: 'sonar_token')]){
       source_root = get_source_root(source_root)
-      def urlCommand = "sed -i -e 's,SONARQUBE,${sonarqube}' package.json"
-      def tokenCommand = "sed -i -e 's/SONAR_TOKEN/${sonar_token}/g' package.json"
+      def urlCommand = "sed -i -e \'s,SONARQUBE,${sonarqube}\' package.json"
+      def tokenCommand = "sed -i -e \'s/SONAR_TOKEN/${sonar_token}\' package.json"
       sh "${urlCommand} && ${tokenCommand}"
       dir("${source_root}") {
         sh "npm run sonar"
